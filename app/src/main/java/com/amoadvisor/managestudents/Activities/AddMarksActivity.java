338ce24d747d9b855d771d85c8785ba2bb8aafc5
Aftab Ali <aaftabali1998@gmail.com>
2019-08-04 09:46:50 +0530

    first

---
+++ app/src/main/java/com/amoadvisor/managestudents/Activities/AddMarksActivity.java
@@ -0,0 +1,15 @@
+package com.amoadvisor.managestudents.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.amoadvisor.managestudents.R;
+
+public class AddMarksActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_marks);
+    }
+}
